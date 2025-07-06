ca54f2ceb7fe10d0840725bd05381b28596a4491
Suhani2305 <pushpendrarawat868@gmail.com>
2025-07-06 12:20:38 +0530

    Added all my fundamental React practice files and examples. Hope this helps anyone learning the basics! 🚀

---
+++ 01. Fundamentals/02. JSX/exercise/solution/WelcomeMessage.jsx
@@ -0,0 +1,25 @@
+// const WelcomeMessage = () => {
+//   return (
+//     <div>
+//       <h1>Hello, World!</h1>
+//       <p>Welcome to learning JSX!</p>
+//     </div>
+//   );
+// };
+
+// export default WelcomeMessage;
+
+//rfce
+ 
+
+function WelcomeMessage() {
+  return (
+    // <section></section>   JSX expressions must have one parent element
+    <div>
+      <h1>Hello, World</h1>
+      <p> welcome to learning</p>
+    </div>
+  )
+}
+
+export default WelcomeMessage
